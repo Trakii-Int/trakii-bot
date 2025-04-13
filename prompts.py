b30cda97b09f21cb650b0ff789603c6caed44664
Carlos Mendieta <57121515+TheWeenhayek@users.noreply.github.com>
2025-04-12 20:56:10 -0400

    Update prompts.py
    Adding list devices functionality

--- prompts.py
+++ prompts.py
@@ -15,7 +15,8 @@ Your job is to classify the following message into one of four categories:
 1. location - When the user asks for location (coordinates or place)
 2. speed - When the user asks for speed
 3. status - When the user asks for device status (online, battery, last update)
-4. ignore - When the user ask whatever that is not related to the previus detailed categories
+4. list - When the user requests to see all registered devices or a list of them
+5. ignore - When the user ask whatever that is not related to the previus detailed categories
 
 Classify the below message into one of these categories.
 
@@ -27,6 +28,7 @@ When the input messages comes on Spanish you have to answer ALWAYS on Spanish
 - Location: {triage_location}
 - Speed: {triage_speed}
 - Status: {triage_status}
+- List: {triage_list}
 - Ignore: {triage_no}
 
 </ Rules >
@@ -36,7 +38,7 @@ Classify the following message:
 """
 
 triage_user_prompt = """
-Please determine how to handle the below message:
+Please determine the type of request for the following message:
 
 {message}
 """
