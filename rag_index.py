44e7a0f6c7f691e8401826f260c5f120ee9aa7d6
Carlos Mendieta <57121515+TheWeenhayek@users.noreply.github.com>
2025-06-11 15:54:57 -0400

    Update rag_index.py

--- rag_index.py
+++ rag_index.py
@@ -2,7 +2,8 @@ from langchain.document_loaders import JSONLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
-
+from dotenv import load_dotenv
+load_dotenv()
 # Carga tu FAQ
 loader = JSONLoader(file_path="faq_trakii.json", jq_schema=".[].answer")
 docs = loader.load()
